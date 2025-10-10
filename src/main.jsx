e6c17afa5e85b86d6860c0454d0dd66fccd236ab
Taha Mesbahi <taha.mesbahi@insa-rouen.fr>
2025-10-11 00:07:27 +0200

    feat: add admin route + render project images/pdf thumbnails

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import App from './App.jsx'
+import Admin from './pages/admin.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>,
 )
