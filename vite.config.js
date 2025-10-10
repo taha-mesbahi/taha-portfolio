5a794d7c4f694ab6c70c53e1af6ed19d1014cb71
Taha Mesbahi <taha.mesbahi@insa-rouen.fr>
2025-10-10 23:53:20 +0200

    fix: force rebuild vite v2 with new assets directory

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins:[react()], base:'/taha-portfolio/' })
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/taha-portfolio/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets_v2', // 🔥 nouveau dossier d’assets pour forcer le refresh
+    emptyOutDir: true,
+  },
+})
